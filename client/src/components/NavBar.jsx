e8fd52f6418508d29c730851f5df523c71be61bf
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-19 10:57:18 +0200

    Styled component with flexbox layout, theme colors, and drop shadow

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+import { MenuIcon } from "@mui/icons-material";
+
+const NavBarDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 16px 40px;
+    align-items: center;
+    color: ${({ theme }) => theme.text_primary};
+    gap: 30px;
+    background: ${({ theme }) => theme.bg};
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(5.7px);
+    -webkit-backdrop-filter: blur(5.7px);
+`;
 
 const NavBar = () => {
-    return <div>NavBar</div>;
+    return <NavBarDiv>
+        <MenuIcon/>
+    </NavBarDiv>;
 };
 
 export default NavBar
