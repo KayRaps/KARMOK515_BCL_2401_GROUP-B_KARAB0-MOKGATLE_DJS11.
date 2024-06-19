8e487d90171ce039c8acfe3cd85f0840d6c70921
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-19 09:30:39 +0200

    Added media query for responsive behavior on screens under 1100px.

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -11,7 +11,15 @@ const MenuContainer = styled.div`
     display: flex;
     background-color: ${({ theme }) => theme.bg};
     color:  ${({ theme }) => theme.text_primary};
-
+     @media (max-width: 1100px) {
+           position: fixed;
+           z-index: 1000;
+           width: 100%;
+           max-width: 250px;
+           left: ${({ setMenuOpen }) => (setMenuOpen ? "0" : "-100%")};
+           transition: 0.3s ease-in-out;
+        }   
+    
 `;
 const Flex = styled.div`
    display: flex;
