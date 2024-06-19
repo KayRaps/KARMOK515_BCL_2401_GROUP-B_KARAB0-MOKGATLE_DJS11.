e8d2c380db1c3809f360328828a2a7e59012a24f
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-19 20:51:59 +0200

    cleaned the code

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { MenuIcon } from "@mui/icons-material";
+import { Menu, PersonRounded } from "@mui/icons-material";
 
 const NavBarDiv = styled.div`
     display: flex;
@@ -10,17 +10,31 @@ const NavBarDiv = styled.div`
     align-items: center;
     color: ${({ theme }) => theme.text_primary};
     gap: 30px;
-    background: ${({ theme }) => theme.bg};
-    border-radius: 16px;
+    background: ${({ theme }) => theme.bgLight};
     box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
     backdrop-filter: blur(5.7px);
     -webkit-backdrop-filter: blur(5.7px);
 `;
 
+const ButtonDiv = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  text-decoration: none;
+  max-width: 70px;
+  align-items: center;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
 const NavBar = () => {
-    return <NavBarDiv>
-        <MenuIcon/>
-    </NavBarDiv>;
+    return (
+      <NavBarDiv>
+        <Menu />
+        <ButtonDiv>
+          <PersonRounded /> 
+          Login
+        </ButtonDiv>
+      </NavBarDiv>
+    );
 };
 
-export default NavBar
+export default NavBar;
