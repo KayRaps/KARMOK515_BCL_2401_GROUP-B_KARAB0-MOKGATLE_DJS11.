bfaed303fa474061490db9124b3a30093feca5bd
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-20 14:54:26 +0200

    Created responsive styled component for Dashboard main area.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-const DashboardMain = styled.div``;
+const DashboardMain = styled.div`
+  padding: 20px 30px;
+  padding-bottom: 200px;
+  height: 100%;
+  overflow-y: scroll;
+  display: flex;
+  gap: 20px;
+  @media (max-width: 768px) {
+    padding: 6px 10px;
+}
+`;
 const FilterContainer = styled.div``;
 const Topic = styled.div``;
 const Span = styled.div``;
