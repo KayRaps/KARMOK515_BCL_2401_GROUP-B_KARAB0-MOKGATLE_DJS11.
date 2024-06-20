d2ae95e5e6ffc9d893b7cf21fc0f6f850126d6c6
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-20 21:27:19 +0200

    Implemented styled Title component with ellipsis overflow.

--- client/src/components/PodcastCard.jsx
+++ client/src/components/PodcastCard.jsx
@@ -77,7 +77,16 @@ const MainInfo = styled.div`
    gap: 4px;
 `;
 
-const Title = styled.div``;
+const Title = styled.div`
+   overflow: hidden;
+   display: -webkit-box;
+   max-width: 100%;
+   -webkit-line-clamp: 2;
+   -webkit-box-orient: vertical;
+   overflow: hidden;
+   text-overflow: ellipsis;
+   color: ${({ theme }) => theme.text_primary};
+`;
 
 const Description = styled.div``;
 
