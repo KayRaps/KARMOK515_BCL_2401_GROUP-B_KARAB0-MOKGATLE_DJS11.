5213292c3c1c2a7da31cbede2e8537448b9f2041
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-06-20 14:43:43 +0200

    Create Dashboard component with styled components for layout: DashboardMain, FilterContainer, Topic, Span, Podcasts. Include "Most Popular" topic with "Show All" link and a Podcasts section.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const DashboardMain = styled.div``;
+const FilterContainer = styled.div``;
+const Topic = styled.div``;
+const Span = styled.div``;
+const Podcasts = styled.div``;
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    return <DashboardMain>
+        <FilterContainer>
+            <Topic>
+                Most Popular
+                <Link>
+                  <Span>Show All</Span>
+                </Link>   
+            </Topic>
+            <Podcasts>Hey</Podcasts>
+        </FilterContainer>
+    </DashboardMain>;
 };
 
 export default Dashboard;
